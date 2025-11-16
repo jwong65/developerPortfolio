a0610bdc2732bd8c6ad47ad8497e298c0e7df2cc
jwong65 <jason.wong6507@gmail.com>
2025-11-16 00:30:31 -0500

    Added the navbar for testing, working with tailwind to have fixed navbar, imported into app

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,14 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Testing Vite with Tailwind CSS.
-      </h1>
+    <div>
+      <Navbar />
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Testing Vite with Tailwind CSS.
+        </h1>
+      </div>
     </div>
   );
 }
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header className='w-full fixed top-0 left-0 bg-white shadow-md z-25'>
+        <nav className='max-w-7xl mx-auto flex items-center justify-between p-4'>
+            <h1 className='text-xl font-bold'>Jason Wong</h1>
+        </nav>
+    </header>
+  )
+}
